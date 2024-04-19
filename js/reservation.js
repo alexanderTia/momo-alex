48a0e201033d733124042053638ab6adacf79700
mohamed-943 <163649450+mohamed-943@users.noreply.github.com>
2024-04-19 10:37:13 +0100

    Add files via upload

---
+++ js/reservation.js
@@ -0,0 +1,15 @@
+function Ventes()
+{  
+    document.location.href="../html/reservation.html";
+    document.getElementById("location").style.display="none";
+    document.getElementById("vente").style.display="block";
+   
+
+}
+function Locations()
+{
+    document.getElementById("forminscription").style.display="block";
+    document.getElementById("formconnexion").style.display="none";
+    
+
+}
